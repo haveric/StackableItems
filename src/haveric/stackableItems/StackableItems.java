bdb19e60315518f0b724d7b89f56299329df6e9d
Ryan Breuer <breuerryan@gmail.com>
2012-08-26 15:05:36 -0500

    It helps to start the metrics..

--- src/haveric/stackableItems/StackableItems.java
+++ src/haveric/stackableItems/StackableItems.java
@@ -1,5 +1,6 @@
 package haveric.stackableItems;
 
+import java.io.IOException;
 import java.util.logging.Logger;
 
 import net.milkbowl.vault.permission.Permission;
@@ -7,6 +8,7 @@ import net.milkbowl.vault.permission.Permission;
 import org.bukkit.plugin.PluginManager;
 import org.bukkit.plugin.RegisteredServiceProvider;
 import org.bukkit.plugin.java.JavaPlugin;
+import org.mcstats.Metrics;
 
 public class StackableItems extends JavaPlugin {
 
@@ -15,6 +17,7 @@ public class StackableItems extends JavaPlugin {
 
     private Commands commands = new Commands(this);
 
+    private Metrics metrics;
 
     public void onEnable() {
         PluginManager pm = getServer().getPluginManager();
@@ -29,7 +32,7 @@ public class StackableItems extends JavaPlugin {
 
 
         // Vault
-        setupVault();
+        setupVault(pm);
 
         SIItems.init(this);
         InventoryUtil.init(this);
@@ -40,20 +43,31 @@ public class StackableItems extends JavaPlugin {
         Config.setup();
 
         getCommand(Commands.getMain()).setExecutor(commands);
+
+        setupMetrics();
     }
 
     public void onDisable() {
 
     }
 
-    private void setupVault() {
-        if (getServer().getPluginManager().getPlugin("Vault") == null) {
+    private void setupVault(PluginManager pm) {
+        if (pm.getPlugin("Vault") == null) {
             log.info(String.format("[%s] Vault not found. Permissions disabled.", getDescription().getName()));
             return;
         }
         RegisteredServiceProvider<Permission> permProvider = getServer().getServicesManager().getRegistration(Permission.class);
         if (permProvider != null) {
             Perms.setPerm(permProvider.getProvider());
+        }
+    }
+
+    private void setupMetrics() {
+        try {
+            metrics = new Metrics(this);
+            metrics.start();
+        } catch (IOException e) {
+            e.printStackTrace();
         }
     }
 }
