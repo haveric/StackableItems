72ec3e2f4fe655b638bd5193368e25c7bacfaa5d
Ryan Breuer <breuerryan@gmail.com>
2013-03-11 11:57:31 -0500

    Missed some files.

---
+++ src/haveric/stackableItems/FurnaceXPConfig.java
@@ -0,0 +1,87 @@
+package haveric.stackableItems;
+
+import java.io.File;
+
+import org.bukkit.Material;
+import org.bukkit.configuration.file.FileConfiguration;
+import org.bukkit.configuration.file.YamlConfiguration;
+import org.bukkit.inventory.ItemStack;
+
+public class FurnaceXPConfig {
+
+    private static StackableItems plugin;
+
+    private static FileConfiguration cfgFurnaceXP;
+    private static File cfgFurnaceXPFile;
+
+    private FurnaceXPConfig() { } // Private constructor for utility class
+
+    public static void init(StackableItems si) {
+        plugin = si;
+        cfgFurnaceXPFile = new File(plugin.getDataFolder() + "/furnaceXP.yml");
+        cfgFurnaceXP = YamlConfiguration.loadConfiguration(cfgFurnaceXPFile);
+    }
+
+    public static void reload() {
+        try {
+            cfgFurnaceXP.load(cfgFurnaceXPFile);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void setup() {
+        cfgFurnaceXP.addDefault("DIAMOND", 1.0);
+        cfgFurnaceXP.addDefault("EMERALD", 1.0);
+        cfgFurnaceXP.addDefault("GOLD_INGOT", 1.0);
+
+        cfgFurnaceXP.addDefault("IRON_INGOT", 0.7);
+        cfgFurnaceXP.addDefault("REDSTONE", 0.7);
+
+        cfgFurnaceXP.addDefault("BAKED_POTATO", 0.35);
+        cfgFurnaceXP.addDefault("COOKED_BEEF", 0.35);
+        cfgFurnaceXP.addDefault("COOKED_CHICKEN", 0.35);
+        cfgFurnaceXP.addDefault("COOKED_FISH", 0.35);
+        cfgFurnaceXP.addDefault("GRILLED_PORK", 0.35);
+
+        cfgFurnaceXP.addDefault("CLAY_BRICK", 0.3);
+
+        cfgFurnaceXP.addDefault("INK_SACK 2", 0.2);
+        cfgFurnaceXP.addDefault("INK_SACK 4", 0.2);
+        //cfgFurnaceXP.addDefault("NETHER_QUARTZ", 0.2);
+
+        cfgFurnaceXP.addDefault("COAL 1", 0.15);
+
+        cfgFurnaceXP.addDefault("COAL 0", 0.1);
+        cfgFurnaceXP.addDefault("STONE", 0.1);
+        cfgFurnaceXP.addDefault("NETHER_BRICK", 0.1);
+        cfgFurnaceXP.addDefault("GLASS", 0.1);
+
+        if (!cfgFurnaceXP.isSet("DIAMOND") || !cfgFurnaceXP.isSet("EMERALD") || !cfgFurnaceXP.isSet("GOLD_INGOT") || !cfgFurnaceXP.isSet("IRON_INGOT") || !cfgFurnaceXP.isSet("REDSTONE")
+         || !cfgFurnaceXP.isSet("BAKED_POTATO") || !cfgFurnaceXP.isSet("COOKED_BEEF") || !cfgFurnaceXP.isSet("COOKED_CHICKEN") || !cfgFurnaceXP.isSet("COOKED_FISH") || !cfgFurnaceXP.isSet("GRILLED_PORK")
+         || !cfgFurnaceXP.isSet("CLAY_BRICK") || !cfgFurnaceXP.isSet("INK_SACK 2") || !cfgFurnaceXP.isSet("INK_SACK 4") || !cfgFurnaceXP.isSet("COAL 1")
+         || !cfgFurnaceXP.isSet("COAL 0") || !cfgFurnaceXP.isSet("STONE") || !cfgFurnaceXP.isSet("NETHER_BRICK") || !cfgFurnaceXP.isSet("GLASS")) {
+
+            cfgFurnaceXP.options().copyDefaults(true);
+            Config.saveConfig(cfgFurnaceXP, cfgFurnaceXPFile);
+        }
+    }
+
+    public static int getXP(ItemStack item) {
+        double xp = 0.0;
+        Material mat = item.getType();
+        xp = cfgFurnaceXP.getDouble(mat + " " + item.getDurability(), 0.0);
+
+        if (xp == 0.0) {
+            xp = cfgFurnaceXP.getDouble("" + mat, 0.0);
+        }
+        double totalXp = xp * item.getAmount();
+        int intPart = (int) totalXp;
+        double fracPart = totalXp - intPart;
+        if (Math.random() <= fracPart) {
+            intPart++;
+        }
+
+        return intPart;
+    }
+}
---
+++ src/haveric/stackableItems/fileWriter/CustomFileWriter.java
@@ -0,0 +1,144 @@
+package haveric.stackableItems.fileWriter;
+
+import haveric.stackableItems.StackableItems;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+import org.bukkit.Material;
+
+public class CustomFileWriter {
+    private StackableItems plugin;
+
+    private File defaultFile;
+    private File customFile;
+
+    private File dataFolder;
+
+    private List<Material> matList;
+
+    String fileName;
+
+    public CustomFileWriter(StackableItems si, String name) {
+        plugin = si;
+        fileName = name;
+        reload();
+    }
+
+    public void reload() {
+        if (!getDataFolder().exists()) {
+            getDataFolder().mkdir();
+        }
+
+        File lists = new File(getDataFolder() + "/lists");
+        if (!lists.exists()) {
+            lists.mkdir();
+        }
+    }
+
+    public void reloadFiles(int version, List<Material> list) {
+        defaultFile = new File(getDataFolder() + "/lists/default" + fileName + ".txt");
+        customFile = new File(getDataFolder() + "/lists/custom" + fileName + ".txt");
+
+        createFiles(version, list);
+    }
+
+    private void createFiles(int version, List<Material> list) {
+        if (!defaultFile.exists()) {
+            try {
+                defaultFile.createNewFile();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+        if (!customFile.exists()) {
+            try {
+                customFile.createNewFile();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+
+        try {
+            Scanner defaultScanner = new Scanner(defaultFile);
+
+            if (defaultFile.length() > 0) {
+                defaultScanner.next();
+                int fileVersion = defaultScanner.nextInt();
+                if (fileVersion < version) {
+                    defaultFile.delete();
+                    defaultFile = new File(getDataFolder() + "/lists/default" + fileName + ".txt");
+                    writeList(list, defaultFile, version);
+                }
+            } else {
+                writeList(list, defaultFile, version);
+            }
+            defaultScanner.close();
+
+            Scanner listScanner = new Scanner(defaultFile);
+            matList = new ArrayList<Material>();
+
+            listScanner.next();
+            listScanner.nextInt();
+            while(listScanner.hasNextLine()) {
+                matList.add(Material.getMaterial(listScanner.nextLine()));
+            }
+
+            listScanner.close();
+        } catch (FileNotFoundException e) {
+            plugin.log.warning("default" + fileName + ".txt not found.");
+            e.printStackTrace();
+        }
+
+        try {
+            Scanner customScanner = new Scanner(customFile);
+
+            if (customFile.length() > 0) {
+                matList = new ArrayList<Material>();
+                while (customScanner.hasNextLine()) {
+                    matList.add(Material.getMaterial(customScanner.nextLine()));
+                }
+            }
+
+            customScanner.close();
+        } catch (FileNotFoundException e) {
+            plugin.log.warning("custom" + fileName + ".txt not found.");
+            e.printStackTrace();
+        }
+    }
+
+    private void writeList(List<Material> list, File f, int version) {
+        try {
+            FileWriter fstream = new FileWriter(f);
+            PrintWriter out = new PrintWriter(fstream);
+            out.println("Version: " + version);
+
+            for (Material mat : list) {
+                out.println(mat.name());
+            }
+
+            out.close();
+            fstream.close();
+        } catch (IOException e) {
+            plugin.log.warning(String.format("File %s not found.", f.getName()));
+        }
+    }
+
+    private File getDataFolder() {
+        if (dataFolder == null) {
+            dataFolder = plugin.getDataFolder();
+        }
+
+        return dataFolder;
+    }
+
+    public List<Material> getMatList() {
+        return matList;
+    }
+}
